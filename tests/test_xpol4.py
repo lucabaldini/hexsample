23e0878dcaa1426854f7c2e428bd833e395dc5c0
Luca Baldini <luca.baldini@pi.infn.it>
2024-01-29 06:40:06 +0100

    Minor.

--- tests/test_xpol4.py
+++ tests/test_xpol4.py
@@ -26,22 +26,74 @@ from hexsample.plot import plt
 from hexsample.xpol import XPOL3_SIZE, XPOL_PITCH
 
 
-def display_adc(num_cols: int = 9, num_rows: int = 6, pitch: float = 50.,
-    layout=HexagonalLayout.ODD_Q):
+
+def display_adc_7(num_cols: int = 15, num_rows: int = 18, pitch: float = 50.,
+    layout=HexagonalLayout.EVEN_R):
     """Draw a sketch of the muGPD.
     """
-    plt.figure(f'XPOL 4', figsize=(15., 9.))
+    magic_sequence = (0, 1, 5, 6, 2, 3, 4)
+    start_adc = (0, 2, 5, 4, 2, 1, 4, 6, 1, 3, 6, 0, 3, 5)
+    plt.figure(f'XPOL 4 readout 7', figsize=(9., 9.))
     grid = HexagonalGrid(layout, num_cols, num_rows, pitch)
     display = HexagonalGridDisplay(grid)
-    display.draw()
+    collection = display.draw()
+    color = np.full((num_cols, num_rows), 'none')
+    for i in range(7):
+        for j in range(14):
+            color[i, j] = '#bbb'
+    for i, j in ((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 1)):
+        color[i, j] = '#888'
+    for pixel in ((12, 2), (9, 3), (12, 6), (9, 7), (13, 11), (9, 11), (13, 15)):
+        color[pixel] = '#bbb'
+        for i, j in grid.neighbors(*pixel):
+            color[i, j] = '#bbb'
+    collection.set_facecolor(color.transpose().flatten())
+    for col in np.arange(num_cols):
+        for row in np.arange(num_rows):
+            x, y = grid.pixel_to_world(col, row)
+            start = magic_sequence.index(start_adc[row % len(start_adc)])
+            index = (col + start) % len(magic_sequence)
+            adc_index = magic_sequence[index]
+            plt.text(x, y, f'{adc_index}', ha='center', va='center', size='small')
+    plt.margins(0.01, 0.01)
+    display.setup_gca()
+
+def display_adc_9(num_cols: int = 15, num_rows: int = 18, pitch: float = 50.,
+    layout=HexagonalLayout.EVEN_R):
+    """Draw a sketch of the muGPD.
+    """
+    plt.figure(f'XPOL 4 readout 9', figsize=(9., 9.))
+    grid = HexagonalGrid(layout, num_cols, num_rows, pitch)
+    display = HexagonalGridDisplay(grid)
+    collection = display.draw()
+    color = np.full((num_cols, num_rows), 'none')
+    for i, j in ((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)):
+        color[i, j] = '#888'
+    for pixel in ((2, 5), (6, 5), (10, 5), (2, 9), (6, 9), (10, 9), (6, 13)):
+        color[pixel] = '#bbb'
+        for i, j in grid.neighbors(*pixel):
+            color[i, j] = '#bbb'
+    collection.set_facecolor(color.transpose().flatten())
     for col in np.arange(num_cols):
         for row in np.arange(num_rows):
             x, y = grid.pixel_to_world(col, row)
             adc_index = (col % 3 + row * 3) % 9
-            plt.text(x, y, f'{adc_index}', ha='center', va='center')
+            plt.text(x, y, f'{adc_index}', ha='center', va='center', size='small')
+    plt.margins(0.01, 0.01)
+    display.setup_gca()
+
+def display_template(num_cols: int = 16, num_rows: int = 12, pitch: float = 50.,
+    layout=HexagonalLayout.ODD_Q):
+    plt.figure(f'Hexagonal template', figsize=(15., 9.))
+    grid = HexagonalGrid(layout, num_cols, num_rows, pitch)
+    display = HexagonalGridDisplay(grid)
+    display.draw()
     plt.margins(0.01, 0.01)
     display.setup_gca()
 
 
+
 if __name__ == '__main__':
-    display_adc()
+    display_adc_7()
+    display_adc_9()
+    #display_template()
