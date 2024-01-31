665bedd54df14246b2461f7e832072987c0c6c8b
Luca Baldini <luca.baldini@pi.infn.it>
2024-01-31 14:58:18 +0100

    Minor.

--- hexsample/buffer.py
+++ hexsample/buffer.py
@@ -20,30 +20,39 @@
 """Buffer class for chip readout and related quantities
 """
 
-import numpy as np 
+import numpy as np
+
+from hexsample import rng
+from hexsample import logger
+from hexsample.plot import plt, setup_gca
+
 
 class Buffer:
+
     """Buffer class constructor
     """
+
     def __init__(self, T_SERVICE: float, MAX_LENGHT: int, N_PROCESSORS: int, queue_lenght: int=0):
         self.T_SERVICE = T_SERVICE #/mus
         self.MAX_LENGHT = MAX_LENGHT
         self.N_PROCESSORS = N_PROCESSORS
         self.queue_lenght = queue_lenght
-    
+
     def __str__(self):
-        formatted_print=f'Element of the class {self.__class__.__name__}\n \
-        Features of the buffer:
-        Service time : {self.T_SERVICE} mus\n \
-        Lenght of the buffer : {self.MAX_LENGHT}\n \
-        Number of processors per buffer : {self.N_PROCESSORS}'
+        """
+        """
+        formatted_print = f'Element of the class {self.__class__.__name__}\n' \
+        'Features of the buffer:' \
+        'Service time : {self.T_SERVICE} mus\n' \
+        'Lenght of the buffer : {self.MAX_LENGHT}\n' \
+        'Number of processors per buffer : {self.N_PROCESSORS}'
         return formatted_print
 
     def store_or_reject(self, n_input_evts: int=1) -> int:
         """This function checks the lenght of the queue and decides if an input event
-        is stored or discarded due to full queue. 
+        is stored or discarded due to full queue.
         It updates the lenght of the queue if needed and returns the number
-        of discarded events in order keep track of the total 'dead' evts. 
+        of discarded events in order keep track of the total 'dead' evts.
         """
         empty_queue_spaces = (self.MAX_LENGHT-self.queue_lenght)
         if n_input_evts <= empty_queue_spaces:
@@ -55,11 +64,49 @@ class Buffer:
             #filling all empty spaces and return the number unaccepted evts
             self.queue_lenght+=(self.MAX_LENGHT-self.queue_lenght)
             return n_input_evts - (self.MAX_LENGHT-self.queue_lenght)
-        
+
     def queue_diminishment(self, n_served_evts: int=1):
         if self.queue_lenght - n_served_evts < 0:
             self.queue_lenght=0
             return
-        else: 
+        else:
             self.queue_lenght -= n_served_evts
             return
+
+
+
+def test_buffer():
+    """
+    """
+
+    total_time = 0.0001
+    average_rate = 250000.
+    event_size = 100
+    readout_thruput = 100.e6
+    service_time = event_size / readout_thruput
+
+    rng.initialize()
+    num_events = rng.generator.poisson(average_rate * total_time)
+    rate = num_events / total_time
+    logger.info(f'Service time: {service_time} s')
+    logger.info(f'About to generate {num_events} event(s)...')
+    dt_arrival = rng.generator.exponential(1. / rate, size=num_events)
+    t_arrival = np.cumsum(dt_arrival)
+    t_service = np.arange(service_time, total_time, service_time)
+
+    t = np.concatenate((t_arrival, t_service))
+    w = np.concatenate((np.full(t_arrival.shape, 1), np.full(t_service.shape, -1)))
+    i = np.argsort(t)
+    t = t[i]
+    w = w[i]
+
+    plt.figure('Buffer timeline')
+    plt.bar(t_arrival, 1., width=1.e-8, color='black')
+    plt.bar(t_service, -1., width=1.e-8, color='red')
+    plt.plot(t, np.cumsum(w))
+    setup_gca(xmin=0., xmax=total_time, xlabel='Time [s]', ymin=-2., ymax=20.)
+
+
+if __name__ == '__main__':
+    test_buffer()
+    plt.show()
