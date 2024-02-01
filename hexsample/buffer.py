6befa041c3552910a8929e573966f1c9a861e0c7
chiara <chiara.it98@gmail.com>
2024-02-01 18:43:53 +0100

    implemented Readout class and its components

--- hexsample/buffer.py
+++ hexsample/buffer.py
@@ -43,7 +43,9 @@ class EventGenerator:
         # every event. 
         rng.initialize()
         num_events = rng.generator.poisson(self.average_rate * self.total_time)
+        logger.info(f'About to generate {num_events} event(s)...')
         rate = num_events / self.total_time
+        logger.info(f'Effective event rate: {rate}')
         #logger.info(f'Service time: {self.service_time} s')
         #logger.info(f'About to generate {num_events} event(s)...')
         #delta between events are generated with an exponential and then 
@@ -97,15 +99,36 @@ class Buffer:
         f'Number of processors per buffer : {self._n_processors}'
         return formatted_print
     
-    def event_processing(self, events: np.array):
+    def rho(self, event_times):
+        """This quantity is the relevant quantity in queuing theory. 
+        It is computed as (rate of evts entering buffer)/(rate of evts exiting buffer).
+        It has been kept the notation in Data Analysis Techniques for High-Energy Phisics
+        (R. Fruhwirth et al.) - Section 1.3
+        It is supposed that the event_times array has been already 'filtered' by
+        readout after trigger.
+        """
+        lambd = np.mean(1/(np.diff(event_times)))
+        mu = 1/self._t_service
+        return lambd/mu
+    
+    def event_processing(self, event_times: np.array, total_time: float):
         """Given an array (or array-like) in input that contains event times, 
         this function transforms the array in an array that contains +1/-1
         that means evt in entrance/exit of buffer.
         After that, the function scans the array and computes for every step
         the queue lenght and updates the dead events count. 
         """
-        
-        for evt in events:
+        #Creating the array with the right features for being processed:
+        #We want an array of +1 and -1, that indicates the arrival and exit
+        #of an event from the buffer. 
+        service_times = np.arange(self._t_service, max(max(event_times),total_time), self._t_service) #ask if it is fine
+        times = np.concatenate((event_times, service_times))
+        idxes = np.argsort(times)
+        times = times[idxes]
+        w = np.concatenate((np.full(event_times.shape, 1), np.full(service_times.shape, -1)))
+        w = w[idxes] #this is the array to give as input to the buffer.
+
+        for evt in times:
             if evt > 0: #events in input
                 if self.queue[-1] < self._max_queue_lenght: #all ok
                     self.queue.append(self.queue[-1]+evt)
@@ -118,113 +141,69 @@ class Buffer:
                 else:
                     self.queue.append(self.queue[-1]+evt)
         return
-
-def readout_after_trigger(event_times: np.array, system_deadtime: float):
-    """This function takes as input the event times. considering the system dead
-    time, it cancels the times of the unread events and counts how many they are.
-    It returns the list of times and the number of dead events.
-    """
-    last_read_event_idx = 0
-    n_dead_events = 0
-    read_events_times = [event_times[last_read_event_idx]]
-    for idx, evt_time in enumerate(event_times[1:]):
-        if (event_times[idx]-event_times[last_read_event_idx]) <= system_deadtime: #the event cannot be read, counted as dead
-            n_dead_events+=1
-        else: #the event time has to be saved because has been read so it is passed to buffer
-            read_events_times.append(evt_time) #event saved
-            last_read_event_idx = idx #changing the index of the last event read, is the new reference point
-    t_arrival_read = np.array(read_events_times)
-
-    return t_arrival_read, n_dead_events
     
+class Readout: #probably useless..l let's think about it
+    #maybe not, it summarizes all the important quantities of the readout. 
+    def __init__(self, triggerreadout: TriggerReadout, buffer: Buffer):
+        self.triggerreadout = triggerreadout
+        self.buffer = buffer
 
-def test_buffer(total_time: float, average_rate: float, reading_time: float, service_time: float, buffer_lenght: int=1):
-    """This is a test for a single buffer readout. 
-    """
+    def effective_rate_entering_buffer(self, event_times: np.array):
+        event_times_to_buffer = self.triggerreadout.readout_after_trigger(event_times)[0]
+        return np.mean(1/(np.diff(event_times_to_buffer)))
+
+    def deadevts_before_buffer(self, event_times: np.array):
+        return self.triggerreadout.readout_after_trigger(event_times)[1]
     
-    #Generating the number of events with a Poissonian distribution
-    # and then (with the true rate), generating times of arrival of
-    # every event. 
-    rng.initialize()
-    num_events = rng.generator.poisson(average_rate * total_time)
-    rate = num_events / total_time
-    logger.info(f'Service time: {service_time} s')
-    logger.info(f'About to generate {num_events} event(s)...')
-    #delta between events are generated with an exponential and then 
-    #summed over in order to obtain absolute times in s.
-    dt_arrival = rng.generator.exponential(1. / rate, size=num_events)
-    t_arrival = np.cumsum(dt_arrival)
-    #After creating time events, those need to be cut if their lag is less than reading time
-    t_arrival_read, num_dead_events = readout_after_trigger(t_arrival, reading_time)
-    dt_arrival_read = np.diff(t_arrival_read, prepend=0)
-    average_rate_entering_buffer = 1/(np.mean(dt_arrival_read))
-    print(f'THE EFFECTIVE RATE ENTERING BUFFER IS: {average_rate_entering_buffer}')
-    f_accepted_events_after_trigger = (len(t_arrival_read))/len(t_arrival)
-    #Creating the set of service times of the buffer
-    t_service = np.arange(service_time, max(max(t_arrival),total_time), service_time) #ask if it is fine
-
-    #Creating the array with the right features for being processed:
-    #We want an array of +1 and -1, that indicates the arrival and exit
-    #of an event from the buffer. 
-    t = np.concatenate((t_arrival_read, t_service))
-    i = np.argsort(t)
-    t = t[i]
-    w = np.concatenate((np.full(t_arrival_read.shape, 1), np.full(t_service.shape, -1)))
-    w = w[i] #this is the array to give as input to the buffer.
-    '''
-    plt.figure('Buffer timeline')
-    plt.bar(t_arrival, 1., width=1.e-8, color='black')
-    plt.bar(t_service, -1., width=1.e-8, color='red')
-    plt.plot(t, np.cumsum(w))
-    setup_gca(xmin=0., xmax=total_time, xlabel='Time [s]', ymin=-2., ymax=20.)
-    '''
-    #Instantiating the buffer
-    buffer = Buffer(t_service=service_time, max_queue_lenght=buffer_lenght, n_dead_evts=0)
-    buffer.queue=[0] #ask why it is necessary
-    buffer.event_processing(w)
-    queue_lenght = len(buffer.queue)
-    dead_events = buffer.n_dead_evts
-    f_accepted_events = (len(t_arrival_read) - dead_events)/len(t_arrival_read)
-    print(f'Number of dead events for {average_rate} Hz rate with buffer lenght {buffer._max_queue_lenght}: {dead_events}')
+    def fraction_deadevts_before_buffer(self, event_times: np.array):
+        return (self.triggerreadout.readout_after_trigger(event_times)[1])/(len(event_times))
     
-    t=np.insert(t,0,0) #this is needed for having len(t) == len(queue) (t lacks of the 0 point)
-    '''
-    Summary plots
-    plt.figure('Buffer timeline')
-    plt.grid()
-    plt.bar(t_arrival, 1., width=1.e-8, color='black')
-    plt.bar(t_service, -1., width=1.e-8, color='red')
-    plt.plot(t, buffer.queue)
-    setup_gca(xmin=0., xmax=total_time, xlabel='Time [s]', ymin=-2., ymax=10.)
-    '''
-    return f_accepted_events_after_trigger, f_accepted_events
-
-
-if __name__ == '__main__':
+    def deadevts_after_buffer(self, event_times: np.array, total_time: float):
+        event_times_to_buffer = self.triggerreadout.readout_after_trigger(event_times)[0]
+        self.buffer.event_processing(event_times_to_buffer, total_time)
+        return self.buffer.n_dead_events
+    
+    def fraction_deadevts_after_buffer(self, event_times: np.array, total_time: float):
+        event_times_to_buffer = self.triggerreadout.readout_after_trigger(event_times)[0]
+        self.buffer.event_processing(event_times_to_buffer, total_time)
+        return (self.buffer.n_dead_evts)/(len(event_times_to_buffer))
+    
+    def fraction_total_dead_evts(self, event_times: np.array, total_time: float):
+        event_times_to_buffer = self.triggerreadout.readout_after_trigger(event_times)[0]
+        self.buffer.event_processing(event_times_to_buffer, total_time)
+        return (len(event_times)-(self.deadevts_after_buffer(event_times)+\
+                                  self.deadevts_before_buffer(event_times)))/len(event_times)
+    
+if __name__ == "__main__":
     #Defining the features of the system
-    total_time = 10 #s
+    total_time = 1 #s
     event_size = 116*2 #bit/evt (116 bits is the expected event size, the *2 is for conservativeness)
-    #reading_time = 4e-6 #s
     reading_time=4e-6 #s
-    #readout_thruput = (50.e6)/8 #bits/s
     readout_thruput = (500.e6)/8 #bits/s
     service_time = event_size / readout_thruput #s
     #Defining the grid of values that we want to simulate
     average_rate = [1e3, 10e3, 50e3, 70e3, 100e3, 150e3, 200e3] #Hz
     max_buffer_lenght = [1, 2, 4, 8]
-    plt.figure()
-    for buffer_lenght in max_buffer_lenght:
-        fractions_before_buffer = []
-        fractions_after_buffer = []
+    #Defining the Readout objects containing the specifics
+    readouts = []
+    for buff_lenght in max_buffer_lenght:
+        #In the following list there is a Readout object for buffer lenght
+        readouts.append(Readout(TriggerReadout(reading_time), Buffer(service_time,buff_lenght)))
+    for readout in readouts:
+        logger.info(f'Service time: {readout.buffer._t_service} s')
+        fdead_events_bb = [] #dead evts before buffer
+        fdead_events_ab = [] #dead evts after buffer
         for rate in average_rate:
-            f1, f2 = test_buffer(total_time, rate, reading_time, service_time, buffer_lenght)
-            fractions_before_buffer.append(f1)
-            fractions_after_buffer.append(f2)
-        plt.errorbar(average_rate, fractions_after_buffer, marker='.', label=f'Buffer lenght = {buffer_lenght}')
-    
+            eventgenerator = EventGenerator(rate, total_time)
+            t_events = eventgenerator.event_times_creation()
+            fdead_events_bb.append(readout.fraction_deadevts_before_buffer(t_events))
+            fdead_events_ab.append(readout.fraction_deadevts_after_buffer(t_events, total_time))
+        plt.figure('Fraction of dead events after 1st stage of readout')
+        plt.title('Fraction of dead events after 1st stage of readout')
+        plt.grid()
+        plt.plot(average_rate, fdead_events_bb, label=f'Buffer lenght = {readout.buffer._max_queue_lenght}')
+        plt.xlabel('Rate of events [Hz]')
+        plt.ylabel('Fraction of dead events')
 
-    plt.grid()
-    plt.xlabel('Average event rate')
-    plt.ylabel('Fraction of events elaborated by buffer')
     plt.legend()
     plt.show()
