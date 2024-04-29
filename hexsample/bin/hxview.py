c0be1ef6d9e576d5927bce7ce39350d5cd4712f0
chiara <chiara.it98@gmail.com>
2024-04-29 17:08:06 +0200

    minor

--- hexsample/bin/hxview.py
+++ hexsample/bin/hxview.py
@@ -27,8 +27,7 @@ import numpy as np
 
 from hexsample.app import ArgumentParser
 from hexsample.fileio import ReconInputFile
-from hexsample.hist import Histogram1d, Histogram2d
-from hexsample.plot import plt, setup_gca
+from hexsample.plot import plt
 from hexsample.analysis import create_histogram
 
 
@@ -39,45 +38,26 @@ __description__ = \
 # Parser object.
 HXVIEW_ARGPARSER = ArgumentParser(description=__description__)
 HXVIEW_ARGPARSER.add_infile()
+HXVIEW_ARGPARSER.add_argument("attribute", type=str, help='Attribute to be viewed.\
+                               To be taken from\ the following list:\
+                               trigger_id (int),  timestamp (float),  livetime (int)  \
+                               roi_size (int), energy (float), position (Tuple[float,float])\
+                               cluster_size (int), roi_size (int)')
+HXVIEW_ARGPARSER.add_argument("mc_table", type=str, help='Tells if the quantities are in mc table\
+                              accepts True or False.')
 
 def hxview(**kwargs):
     """View the file content.
-    Shows histograms of energy and cluster_size of recon events vs their MC truth.
+    Shows histograms of energy and cluster_size.
     """
     input_file = ReconInputFile(kwargs['infile'])
-    # Plotting the reconstructed energy and the true energy
-    histo = create_histogram(input_file, 'energy', mc = False)
-    mc_histo = create_histogram(input_file, 'energy', mc = True)
-    plt.figure('Photons energy')
-    histo.plot(label='Reconstructed')
-    mc_histo.plot(label='MonteCarlo')
-    plt.xlabel('Energy [eV]')
-    plt.legend()
-
-    # Plotting the reconstructed x and y position and the true position.
-    plt.figure('Reconstructed photons position')
-    binning = np.linspace(-5. * 0.1, 5. * 0.1, 100)
-    x = input_file.column('posx')
-    y = input_file.column('posy')
-    histo = Histogram2d(binning, binning).fill(x, y)
+    attribute = kwargs['attribute']
+    is_mc = literal_eval(kwargs['mc_table'])
+    print(is_mc)
+    histo = create_histogram(input_file, attribute, mc = is_mc)
+    plt.figure()
     histo.plot()
-    setup_gca(xlabel='x [cm]', ylabel='y [cm]')
-    plt.figure('True photons position')
-    x_mc = input_file.mc_column('absx')
-    y_mc = input_file.mc_column('absy')
-    histo_mc = Histogram2d(binning, binning).fill(x_mc, y_mc)
-    histo_mc.plot()
-    setup_gca(xlabel='x [cm]', ylabel='y [cm]')
-    #Closing the file and showing the figures.
-    plt.figure('x-direction resolution')
-    binning = np.linspace((x-x_mc).min(), (x-x_mc).max(), 100)
-    histx = Histogram1d(binning, xlabel=r'$x - x_{MC}$ [cm]').fill(x-x_mc)
-    histx.plot()
-    plt.figure('y-direction resolution')
-    binning = np.linspace((y-y_mc).min(), (y-y_mc).max(), 100)
-    histy = Histogram1d(binning, xlabel=r'$y - y_{MC}$ [cm]').fill(y-y_mc)
-    histy.plot()
-
+    plt.xlabel(attribute)
     input_file.close()
     plt.show()
 
