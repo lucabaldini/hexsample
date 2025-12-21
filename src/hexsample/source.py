57a96db87025cf1344fe823e293bf40fa6d21cd7
Luca Baldini <luca.baldini@pi.infn.it>
2025-12-21 15:15:02 +0100

    Yet more refactoring.

--- src/hexsample/source.py
+++ src/hexsample/source.py
@@ -22,7 +22,7 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Optional, Tuple, Union
+from typing import Tuple, Union
 
 import numpy as np
 import xraydb
@@ -30,253 +30,6 @@ from aptapy.plotting import plt, setup_gca
 
 from . import rng
 from .hexagon import HexagonalGrid
-
-
-class AbstractBeamBase(ABC):
-
-    """Abstract base class for all the X-ray beam shapes.
-
-    This defines a single abstract method, :meth:`rvs`, that must be implemented
-    in all concrete subclasses, and should return random photon positions in the
-    xy plane with the proper distribution.
-
-    Note all the positios in this context are expressed in cm.
-    """
-
-    @abstractmethod
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Do-nothing hook to generate random positions in the x-y plane.
-
-        Concrete subclasses must implement this method.
-
-        Arguments
-        ---------
-        size : int
-            The number of X-ray photon positions to be generated.
-
-        Returns
-        -------
-        x, y : 2-element tuple of np.ndarray of shape ``size``
-            The photon positions on the x-y plane.
-        """
-
-
-@dataclass(slots=True)
-class PointBeamSpec:
-
-    """Specification for a point-like X-ray beam.
-
-    Arguments
-    ---------
-    x0 : float
-        The x-coordinate of the beam centroid in cm.
-
-    y0 : float
-        The y-coordinate of the beam centroid in cm.
-    """
-
-    x0: float = 0.
-    y0: float = 0.
-
-
-@dataclass
-class PointBeam(PointBeamSpec, AbstractBeamBase):
-
-    """Point-like X-ray beam.
-    """
-
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Overloaded method.
-        """
-        # pylint: disable=invalid-name
-        x = np.full(size, self.x0)
-        y = np.full(size, self.y0)
-        return x, y
-
-
-@dataclass(slots=True)
-class DiskBeamSpec:
-
-    """Specification for a uniform disk X-ray beam.
-
-    Arguments
-    ---------
-    x0 : float
-        The x-coordinate of the beam centroid in cm.
-
-    y0 : float
-        The y-coordinate of the beam centroid in cm.
-
-    radius : float
-        The disk radius in cm.
-    """
-
-    # pylint: disable=invalid-name
-    x0: float = 0.
-    y0: float = 0.
-    radius: float = 0.1
-
-
-@dataclass
-class DiskBeam(DiskBeamSpec, AbstractBeamBase):
-
-    """Uniform disk X-ray beam.
-    """
-
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Overloaded method.
-        """
-        # pylint: disable=invalid-name
-        r = self.radius * np.sqrt(rng.generator.uniform(size=size))
-        theta = rng.generator.uniform(0., 2. * np.pi, size=size)
-        x = self.x0 + r * np.cos(theta)
-        y = self.y0 + r * np.sin(theta)
-        return x, y
-
-
-@dataclass(slots=True)
-class GaussianBeamSpec:
-
-    """Specification for an azimuthally-symmetric gaussian beam.
-
-    Arguments
-    ---------
-    x0 : float
-        The x-coordinate of the beam centroid in cm.
-
-    y0 : float
-        The y-coordinate of the beam centroid in cm.
-
-    sigma : float
-        The beam sigma in cm.
-    """
-
-    # pylint: disable=invalid-name
-    x0: float = 0.
-    y0: float = 0.
-    sigma: float = 0.1
-
-
-@dataclass
-class GaussianBeam(GaussianBeamSpec, AbstractBeamBase):
-
-    """Azimuthally-simmetric gaussian beam.
-    """
-
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Overloaded method.
-        """
-        # pylint: disable=invalid-name
-        x = rng.generator.normal(self.x0, self.sigma, size=size)
-        y = rng.generator.normal(self.y0, self.sigma, size=size)
-        return x, y
-
-
-@dataclass(slots=True)
-class TriangularBeamSpec:
-
-    """Specification for a triangular uniform X-ray beam.
-
-    Arguments
-    ---------
-    x0 : float
-        The x-coordinate of the first vertex of the triangle in cm.
-
-    y0 : float
-        The y-coordinate of the first vertex of the triangle in cm.
-
-    v1 : Tuple[float, float]
-        The (x, y) coordinates of the second vertex of the triangle in cm.
-
-    v2 : Tuple[float, float]
-        The (x, y) coordinates of the third vertex of the triangle in cm.
-    """
-
-    # pylint: disable=invalid-name
-    x0: float = 0.
-    y0: float = 0.
-    v1: Tuple[float, float] = (1., 0.)
-    v2: Tuple[float, float] = (0., 1.)
-
-
-@dataclass
-class TriangularBeam(TriangularBeamSpec,AbstractBeamBase):
-
-    """Triangular uniform X-ray beam.
-    """
-
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Overloaded method.
-        """
-        if len(self.v1) != 2 or len(self.v2) != 2:
-            raise ValueError("v1 and v2 must have 2 elements.")
-
-        v0_ar = np.array([self.x0, self.y0])
-        v1_ar = np.array(self.v1)
-        v2_ar = np.array(self.v2)
-
-        u = rng.generator.uniform(0, 1, (size, 2))
-        mask = u[:, 0] + u[:, 1] > 1
-        u[mask, :] = 1 - u[mask, :]
-
-        w = (v1_ar - v0_ar) * u[:, 0, None] + (v2_ar - v0_ar) * u[:, 1, None] + v0_ar
-        return w[:, 0], w[:, 1]
-
-
-@dataclass(slots=True)
-class HexagonalBeamSpec:
-
-    """Specification for a hexagonal uniform X-ray beam.
-
-    Arguments
-    ---------
-    x0 : float
-        The x-coordinate of the center of the hexagon in cm.
-
-    y0 : float
-        The y-coordinate of the center of the hexagon in cm.
-
-    v0 : Tuple[float, float]
-        The (x, y) coordinates of the first vertex of the hexagon in cm.
-
-    v1 : Tuple[float, float]
-        The (x, y) coordinates of the second vertex of the hexagon in cm.
-    """
-
-    # pylint: disable=invalid-name
-    x0: float = 0.
-    y0: float = 0.
-    v0: Tuple[float, float] = (1., 0.)
-    v1: Tuple[float, float] = (0., 1.)
-
-
-@dataclass
-class HexagonalBeam(HexagonalBeamSpec, AbstractBeamBase):
-
-    """Hexagonal uniform X-ray beam.
-    """
-
-    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
-        """Overloaded method.
-        """
-        _, size_t = np.unique(rng.generator.integers(0, 6, size), return_counts=True)
-        x = np.zeros(size)
-        y = np.zeros(size)
-
-        j = 0
-        c = np.array([self.x0, self.y0])
-        for i, t_s in enumerate(size_t):
-            rotator = HexagonalGrid.create_rotator(np.pi / 3. * i)
-            v0_rot = rotator((self.v0[0] - c[0], self.v0[1] - c[1])) + c
-            v1_rot = rotator((self.v1[0] - c[0], self.v1[1] - c[1])) + c
-            beam = TriangularBeam(self.x0, self.y0, tuple(v0_rot), tuple(v1_rot))
-            x_tr, y_tr = beam.rvs(t_s)
-
-            x[j:j + t_s] = x_tr
-            y[j:j + t_s] = y_tr
-            j += t_s
-
-        return x, y
 
 
 class AbstractSpectrumBase(ABC):
@@ -318,7 +71,7 @@ class AbstractSpectrumBase(ABC):
 @dataclass(slots=True)
 class LineSpec:
 
-    """Specification for a monochromatic emission line at a given energy.
+    """Specifications for a monochromatic emission line at a given energy.
     """
 
     energy: float = 6000.0
@@ -347,7 +100,7 @@ class Line(LineSpec, AbstractSpectrumBase):
 @dataclass(slots=True)
 class LineForestSpec:
 
-    """Specification for a set of X-ray emission lines for a given element and
+    """Specifications for a set of X-ray emission lines for a given element and
     initial level.
 
     See https://xraypy.github.io/XrayDB/python.html#x-ray-emission-lines for
@@ -412,46 +165,296 @@ class LineForest(LineForestSpec, AbstractSpectrumBase):
         return f"{self.line_dict}"
 
 
-class Source:
+# Type alias for the specification of the source spectrum.
+SpectrumSpec = Union[
+    LineSpec,
+    LineForestSpec
+    ]
 
-    """Base class for a X-ray source.
 
-    Arguments
-    ---------
-    rate : float
-        The photon rate in Hz.
+class AbstractBeamBase(ABC):
 
-    spectrum : SpectrumBase
-        The source spectrum.
+    """Abstract base class for all the X-ray beam shapes.
 
-    beam : AbstractBeamBase
-        The source beam morphology.
+    This defines a single abstract method, :meth:`rvs`, that must be implemented
+    in all concrete subclasses, and should return random photon positions in the
+    xy plane with the proper distribution.
+
+    Note all the positios in this context are expressed in cm.
     """
 
-    def __init__(self, spectrum: AbstractSpectrumBase, beam: AbstractBeamBase, rate: float = 100.) -> None:
-        """Constructor.
-        """
-        self.spectrum = spectrum
-        self.beam = beam
-        self.rate = rate
+    @abstractmethod
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Do-nothing hook to generate random positions in the x-y plane.
 
-    def rvs_timestamp(self, size: int = 1, tmin: float = 0.) -> np.ndarray:
-        """Extract random times.
+        Concrete subclasses must implement this method.
 
         Arguments
         ---------
         size : int
-            The number of X-ray timestamps to be generated.
+            The number of X-ray photon positions to be generated.
 
         Returns
         -------
-        timestamp : np.ndarray of shape ``size``
-            The photon timestamps in eV.
+        x, y : 2-element tuple of np.ndarray of shape ``size``
+            The photon positions on the x-y plane.
         """
-        tmax = tmin + size / self.rate
-        timestamp = rng.generator.uniform(tmin, tmax, size)
-        timestamp.sort()
-        return timestamp
+
+
+@dataclass(slots=True)
+class PointBeamSpec:
+
+    """Specifications for a point-like X-ray beam.
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the beam centroid in cm.
+
+    y0 : float
+        The y-coordinate of the beam centroid in cm.
+    """
+
+    x0: float = 0.
+    y0: float = 0.
+
+
+@dataclass
+class PointBeam(PointBeamSpec, AbstractBeamBase):
+
+    """Point-like X-ray beam.
+    """
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Overloaded method.
+        """
+        # pylint: disable=invalid-name
+        x = np.full(size, self.x0)
+        y = np.full(size, self.y0)
+        return x, y
+
+
+@dataclass(slots=True)
+class DiskBeamSpec:
+
+    """Specifications for a uniform disk X-ray beam.
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the beam centroid in cm.
+
+    y0 : float
+        The y-coordinate of the beam centroid in cm.
+
+    radius : float
+        The disk radius in cm.
+    """
+
+    # pylint: disable=invalid-name
+    x0: float = 0.
+    y0: float = 0.
+    radius: float = 0.1
+
+
+@dataclass
+class DiskBeam(DiskBeamSpec, AbstractBeamBase):
+
+    """Uniform disk X-ray beam.
+    """
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Overloaded method.
+        """
+        # pylint: disable=invalid-name
+        r = self.radius * np.sqrt(rng.generator.uniform(size=size))
+        theta = rng.generator.uniform(0., 2. * np.pi, size=size)
+        x = self.x0 + r * np.cos(theta)
+        y = self.y0 + r * np.sin(theta)
+        return x, y
+
+
+@dataclass(slots=True)
+class GaussianBeamSpec:
+
+    """Specifications for an azimuthally-symmetric gaussian beam.
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the beam centroid in cm.
+
+    y0 : float
+        The y-coordinate of the beam centroid in cm.
+
+    sigma : float
+        The beam sigma in cm.
+    """
+
+    # pylint: disable=invalid-name
+    x0: float = 0.
+    y0: float = 0.
+    sigma: float = 0.1
+
+
+@dataclass
+class GaussianBeam(GaussianBeamSpec, AbstractBeamBase):
+
+    """Azimuthally-simmetric gaussian beam.
+    """
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Overloaded method.
+        """
+        # pylint: disable=invalid-name
+        x = rng.generator.normal(self.x0, self.sigma, size=size)
+        y = rng.generator.normal(self.y0, self.sigma, size=size)
+        return x, y
+
+
+@dataclass(slots=True)
+class TriangularBeamSpec:
+
+    """Specifications for a triangular uniform X-ray beam.
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the first vertex of the triangle in cm.
+
+    y0 : float
+        The y-coordinate of the first vertex of the triangle in cm.
+
+    v1 : Tuple[float, float]
+        The (x, y) coordinates of the second vertex of the triangle in cm.
+
+    v2 : Tuple[float, float]
+        The (x, y) coordinates of the third vertex of the triangle in cm.
+    """
+
+    # pylint: disable=invalid-name
+    x0: float = 0.
+    y0: float = 0.
+    v1: Tuple[float, float] = (1., 0.)
+    v2: Tuple[float, float] = (0., 1.)
+
+
+@dataclass
+class TriangularBeam(TriangularBeamSpec,AbstractBeamBase):
+
+    """Triangular uniform X-ray beam.
+    """
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Overloaded method.
+        """
+        if len(self.v1) != 2 or len(self.v2) != 2:
+            raise ValueError("v1 and v2 must have 2 elements.")
+
+        v0_ar = np.array([self.x0, self.y0])
+        v1_ar = np.array(self.v1)
+        v2_ar = np.array(self.v2)
+
+        u = rng.generator.uniform(0, 1, (size, 2))
+        mask = u[:, 0] + u[:, 1] > 1
+        u[mask, :] = 1 - u[mask, :]
+
+        w = (v1_ar - v0_ar) * u[:, 0, None] + (v2_ar - v0_ar) * u[:, 1, None] + v0_ar
+        return w[:, 0], w[:, 1]
+
+
+@dataclass(slots=True)
+class HexagonalBeamSpec:
+
+    """Specifications for a hexagonal uniform X-ray beam.
+
+    Arguments
+    ---------
+    x0 : float
+        The x-coordinate of the center of the hexagon in cm.
+
+    y0 : float
+        The y-coordinate of the center of the hexagon in cm.
+
+    v0 : Tuple[float, float]
+        The (x, y) coordinates of the first vertex of the hexagon in cm.
+
+    v1 : Tuple[float, float]
+        The (x, y) coordinates of the second vertex of the hexagon in cm.
+    """
+
+    # pylint: disable=invalid-name
+    x0: float = 0.
+    y0: float = 0.
+    v0: Tuple[float, float] = (1., 0.)
+    v1: Tuple[float, float] = (0., 1.)
+
+
+@dataclass
+class HexagonalBeam(HexagonalBeamSpec, AbstractBeamBase):
+
+    """Hexagonal uniform X-ray beam.
+    """
+
+    def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+        """Overloaded method.
+        """
+        _, size_t = np.unique(rng.generator.integers(0, 6, size), return_counts=True)
+        x = np.zeros(size)
+        y = np.zeros(size)
+
+        j = 0
+        c = np.array([self.x0, self.y0])
+        for i, t_s in enumerate(size_t):
+            rotator = HexagonalGrid.create_rotator(np.pi / 3. * i)
+            v0_rot = rotator((self.v0[0] - c[0], self.v0[1] - c[1])) + c
+            v1_rot = rotator((self.v1[0] - c[0], self.v1[1] - c[1])) + c
+            beam = TriangularBeam(self.x0, self.y0, tuple(v0_rot), tuple(v1_rot))
+            x_tr, y_tr = beam.rvs(t_s)
+
+            x[j:j + t_s] = x_tr
+            y[j:j + t_s] = y_tr
+            j += t_s
+
+        return x, y
+
+
+# Type alias for the specification of the source morphology.
+BeamSpec = Union[
+    PointBeamSpec,
+    DiskBeamSpec,
+    GaussianBeamSpec,
+    TriangularBeamSpec,
+    HexagonalBeamSpec
+    ]
+
+
+@dataclass(slots=True)
+class SourceSpec:
+
+    """Specifications for a fully-fledged X-ray source.
+
+    Arguments
+    ---------
+    spectrum : SpectrumSpec
+        The source spectrum.
+
+    beam : BeamSpec
+        The source morphology.
+
+    rate : float
+        The photon rate in Hz.
+    """
+
+    spectrum: SpectrumSpec
+    beam: BeamSpec
+    rate: float = 100.
+
+
+class Source(SourceSpec):
+
+    """Class describing a X-ray source.
+    """
 
     def rvs(self, size: int = 1) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Extract random X-ray initial properties.
@@ -459,25 +462,18 @@ class Source:
         Arguments
         ---------
         size : int
-            The number of X-ray properties to be generated.
+            The number of X-rays to be generated.
 
         Returns
         -------
-        timestamp, energy, x, y : 4-element tuple of np.ndarray of shape ``size``
-            The photon properties.
+        t, energy, x, y : 4-element tuple of np.ndarray of shape ``size``
+            The X-ray properties.
         """
         # pylint: disable=invalid-name
-        timestamp = self.rvs_timestamp(size)
+        tmin = 0.
+        tmax = tmin + size / self.rate
+        t = rng.generator.uniform(tmin, tmax, size)
+        t.sort()
         energy = self.spectrum.rvs(size)
         x, y = self.beam.rvs(size)
-        return timestamp, energy, x, y
-
-
-
-@dataclass
-class SourceConfiguration:
-
-    """Dataclass to hold source configuration parameters.
-    """
-
-    source_type: str
+        return t, energy, x, y
