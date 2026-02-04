045bdcca273b4ab8b7bf5fdd04f8360210e1e6e4
Augusto Cattafesta <augustocattafesta@gmail.com>
2026-02-04 11:47:37 +0100

    Minor.

--- scripts/resolution.py
+++ scripts/resolution.py
@@ -15,10 +15,15 @@ __description__ = ""
 HXETA_ARGPARSER = argparse.ArgumentParser(description=__description__)
 HXETA_ARGPARSER.add_argument("--enc", type=int, help="equivalent noise charge in electrons")
 HXETA_ARGPARSER.add_argument("--zero_sup_threshold", type=int, help="zero suppression threshold in electrons")
+HXETA_ARGPARSER.add_argument("--save", action="store_true", help="save the figures")
 
 RESOLUTION_DIR = Path.home() / "hexsampledata" / "resolution"
 if not RESOLUTION_DIR.exists():
     RESOLUTION_DIR.mkdir(parents=True, exist_ok=True)
+
+FIGURES_DIR = Path.home() / "hexsample_figures" / "resolution"
+if not FIGURES_DIR.exists():
+    FIGURES_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def distance(input_file_path: str, grid):
@@ -38,6 +43,7 @@ def distance(input_file_path: str, grid):
     input_file.close()
 
     return dr, dr0
+
 
 def _estimate_loc(hist: Histogram2d) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Estimate the location of the distribution in each slice of the histogram.
@@ -84,7 +90,8 @@ def resolution(**kwargs):
     zero_sup_threshold = kwargs["zero_sup_threshold"]
     if enc is None or zero_sup_threshold is None:
         raise RuntimeError("Both enc and zero_sup_threshold arguments are required.")
-    file_prefix = f"simulation_resolution_{enc}enc_zsup{zero_sup_threshold}_hexagonal"
+    zero_sup_thr_simulation = 0
+    file_prefix = f"simulation_resolution_{enc}enc_zsupsim{zero_sup_thr_simulation}_hexagonal"
     simulation_path = RESOLUTION_DIR / f"{file_prefix}.h5"
     # Run simulation if the output file does not already exist
     if not simulation_path.exists():
@@ -93,21 +100,22 @@ def resolution(**kwargs):
                 output_file=str(simulation_path),
                 beam="hexagonal",
                 enc=enc,
-                # This is the cause of the resolution degradation. If in simulate it is set to 30,
+                # This may be the cause of the resolution degradation. If in simulate it is set to 30,
                 # then some events are reconstructed very badly. Furthermore, when setting it to 30,
                 # if the reconstruction is done with a zero suppression threshold of 0, the results
                 # are diffrent from the ones obtained with a threshold of 30, all without noise.
-                zero_sup_threshold=0,
+                zero_sup_threshold=zero_sup_thr_simulation,
                 readout_mode="circular",
                 pitch=0.005,
                 layout=HexagonalLayout.ODD_R,
                 num_cols=304,
                 num_rows=352,
-                gain=1.
+                gain=1.,
+                random_seed=0
         )
     # Reconstruct the simulated file with different algorithms
     # We start with centroid for all pixels
-    all_pixel_centroid_prefix = f"recon_allpix_centroid"
+    all_pixel_centroid_prefix = f"recon_zsuprec{zero_sup_threshold}_allpix_centroid"
     all_pixel_centroid = RESOLUTION_DIR / f"{file_prefix}_{all_pixel_centroid_prefix}.h5"
     if not all_pixel_centroid.exists():
         reconstruct(
@@ -118,7 +126,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="centroid"
         )
     # All pixels reconstructed with the best algorithm (eta for 2 and 3, centroid otherwise)
-    all_pixel_best_prefix = f"recon_allpix_best"
+    all_pixel_best_prefix = f"recon_zsuprec{zero_sup_threshold}_allpix_best"
     all_pixel_best = RESOLUTION_DIR / f"{file_prefix}_{all_pixel_best_prefix}.h5"
     if not all_pixel_best.exists():
         reconstruct(
@@ -129,7 +137,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="eta"
         )
     # Only one-pixel events with centroid
-    one_pixel_centroid_prefix = f"recon_1pix_centroid"
+    one_pixel_centroid_prefix = f"recon_zsuprec{zero_sup_threshold}_1pix_centroid"
     one_pixel_centroid = RESOLUTION_DIR / f"{file_prefix}_{one_pixel_centroid_prefix}.h5"
     if not one_pixel_centroid.exists():
         reconstruct(
@@ -140,7 +148,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="centroid"
         )
     # Only two pixel events with centroid
-    two_pixel_centroid_prefix = f"recon_2pix_centroid"
+    two_pixel_centroid_prefix = f"recon_zsuprec{zero_sup_threshold}_2pix_centroid"
     two_pixel_centroid = RESOLUTION_DIR / f"{file_prefix}_{two_pixel_centroid_prefix}.h5"
     if not two_pixel_centroid.exists():
         reconstruct(
@@ -151,7 +159,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="centroid"
         )
     # Only two-pixel events with eta
-    two_pixel_eta_prefix = f"recon_2pix_eta"
+    two_pixel_eta_prefix = f"recon_zsuprec{zero_sup_threshold}_2pix_eta"
     two_pixel_eta = RESOLUTION_DIR / f"{file_prefix}_{two_pixel_eta_prefix}.h5"
     if not two_pixel_eta.exists():
         reconstruct(
@@ -162,7 +170,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="eta"
         )
     # Only three-pixel events with centroid
-    three_pixel_centroid_prefix = f"recon_3pix_centroid"
+    three_pixel_centroid_prefix = f"recon_zsuprec{zero_sup_threshold}_3pix_centroid"
     three_pixel_centroid = RESOLUTION_DIR / f"{file_prefix}_{three_pixel_centroid_prefix}.h5"
     if not three_pixel_centroid.exists():
         reconstruct(
@@ -173,7 +181,7 @@ def resolution(**kwargs):
             pos_recon_algorithm="centroid"
         )
     # Only three-pixel events with eta
-    three_pixel_eta_prefix = f"recon_3pix_eta"
+    three_pixel_eta_prefix = f"recon_zsuprec{zero_sup_threshold}_3pix_eta"
     three_pixel_eta = RESOLUTION_DIR / f"{file_prefix}_{three_pixel_eta_prefix}.h5"
     if not three_pixel_eta.exists():
         reconstruct(
@@ -185,7 +193,7 @@ def resolution(**kwargs):
         )
     # Define the hexagonal grid for pitch calculation and pixel center determination
     grid = HexagonalGrid()
-    dr_binning = np.linspace(0., 0.4, 101)
+    dr_binning = np.linspace(0., 0.6, 101)
 
     xlabel = "Distance residual [pitch normalized]"
     ylabel = "EEF"
@@ -193,7 +201,7 @@ def resolution(**kwargs):
     dr_1pix, _ = distance(str(one_pixel_centroid), grid)
     hist_1pix = Histogram1d(dr_binning, xlabel=xlabel)
     hist_1pix.fill(dr_1pix)
-    plt.figure("1pix_residuals")
+    fig_1pix = plt.figure("1pix_residuals")
     hew = hist_1pix.ppf(0.5)
     plt.plot(dr_binning, hist_1pix.cdf(dr_binning), "-k", label=f"Centroid (HEW: {hew:.2f})")
     plt.hlines(0.5, dr_binning[0], dr_binning[-1], colors="0.4", linestyles="-.")
@@ -210,39 +218,7 @@ def resolution(**kwargs):
     hist_2pix_eta.fill(dr_2pix_eta)
     hist_2pix_centroid = Histogram1d(dr_binning, xlabel=xlabel)
     hist_2pix_centroid.fill(dr_2pix_centroid)
-
-
-
-
-    # # MOMENTANEOUS PLOT FOR TWO PIXEL EVENTS TAILS
-    # mask = dr_2pix_eta > 0.5
-    # pitch = grid.pitch
-    # input_file = ReconInputFile(two_pixel_eta)
-    # # Monte Carlo true positions
-    # x_mc = input_file.mc_column("absx")
-    # y_mc = input_file.mc_column("absy")
-    # # Monte Carlo true pixel centers
-    # x0, y0 = grid.pixel_to_world(*grid.world_to_pixel(x_mc, y_mc))
-    # x = (input_file.column("posx") - x0) / pitch
-    # y = (input_file.column("posy") - y0) / pitch
-    # id = input_file.column("trigger_id")
-    # mask = id == id[mask][0]
-    # plt.figure("2pix_residuals_scatter")
-    # hist = Histogram2d(np.linspace(-0.6, 0.6, 100), np.linspace(-0.6, 0.6, 100))
-    # xx = (x_mc - x0) / pitch
-    # yy = (y_mc - y0) / pitch
-    # hist.fill(xx, yy)
-    # hist.plot()
-    # plt.scatter(x[mask], y[mask], s=10, alpha=1, color="blue", marker="*")
-    # plt.scatter(xx[mask], yy[mask], s=10, alpha=1, color="green", marker="*")
-    # plt.xlabel("Reconstructed x from pixel center [pitch normalized]")
-    # plt.ylabel("Reconstructed y from pixel center [pitch normalized]")
-    # print(id[mask], dr_2pix_eta[mask])
-    # input_file.close()
-
-
-
-    plt.figure("2pix_residuals")
+    fig_2pix = plt.figure("2pix_residuals")
     hew_eta = hist_2pix_eta.ppf(0.5)
     hew_centroid = hist_2pix_centroid.ppf(0.5)
     plt.plot(dr_binning, hist_2pix_eta.cdf(dr_binning), "-k", label=f"$\\eta$ (HEW: {hew_eta:.2f})")
@@ -261,7 +237,7 @@ def resolution(**kwargs):
     hist_3pix_eta.fill(dr_3pix_eta)
     hist_3pix_centroid = Histogram1d(dr_binning, xlabel=xlabel)
     hist_3pix_centroid.fill(dr_3pix_centroid)
-    plt.figure("3pix_residuals")
+    fig_3pix = plt.figure("3pix_residuals")
     hew_3pix_eta = hist_3pix_eta.ppf(0.5)
     hew_3pix_centroid = hist_3pix_centroid.ppf(0.5)
     plt.plot(dr_binning, hist_3pix_eta.cdf(dr_binning), "-k", label=f"$\\eta$ (HEW: {hew_3pix_eta:.2f})")
@@ -280,7 +256,7 @@ def resolution(**kwargs):
     hist_allpix_best.fill(dr_allpix_best)
     hist_allpix_centroid = Histogram1d(dr_binning, xlabel=xlabel)
     hist_allpix_centroid.fill(dr_allpix_centroid)
-    plt.figure("allpix_residuals")
+    fig_allpix = plt.figure("allpix_residuals")
     hew_allpix_best = hist_allpix_best.ppf(0.5)
     hew_allpix_centroid = hist_allpix_centroid.ppf(0.5)
     plt.plot(dr_binning, hist_allpix_best.cdf(dr_binning), "-k", label=f"Best (HEW: {hew_allpix_best:.2f})")
@@ -350,6 +326,13 @@ def resolution(**kwargs):
     plt.xlabel("Reconstructed distance from pixel center [pitch normalized]")
     plt.ylabel("Mean distance residual [pitch normalized]")
     plt.legend()
+
+    if kwargs["save"]:
+        fig_format = "png"
+        fig_1pix.savefig(FIGURES_DIR / f"1pix_resolution_{enc}enc_{zero_sup_threshold}zsup.{fig_format}", format=fig_format)
+        fig_2pix.savefig(FIGURES_DIR / f"2pix_resolution_{enc}enc_{zero_sup_threshold}zsup.{fig_format}", format=fig_format)
+        fig_3pix.savefig(FIGURES_DIR / f"3pix_resolution_{enc}enc_{zero_sup_threshold}zsup.{fig_format}", format=fig_format)
+        fig_allpix.savefig(FIGURES_DIR / f"allpix_resolution_{enc}enc_{zero_sup_threshold}zsup.{fig_format}", format=fig_format)
 
 
 if __name__ == "__main__":
