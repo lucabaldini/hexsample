04880ae1aedfcb8b88ad2bcad2d832656ec1dffe
Augusto Cattafesta <augustocattafesta@gmail.com>
2026-04-29 17:33:57 +0200

    Handle None DigiEvent for events outside the chip bounds.

--- src/hexsample/fileio.py
+++ src/hexsample/fileio.py
@@ -392,10 +392,11 @@ class DigiOutputFileRectangular(OutputFileBase):
             The Monte Carlo event contribution.
         """
         # pylint: disable=arguments-differ
-        _fill_digi_row_rectangular(self.digi_table.row, digi_event)
-        self.pha_array.append(digi_event.pha.flatten())
-        if mc_event is not None:
-            _fill_mc_row(self.mc_table.row, mc_event)
+        if digi_event is not None:
+            _fill_digi_row_rectangular(self.digi_table.row, digi_event)
+            self.pha_array.append(digi_event.pha.flatten())
+            if mc_event is not None:
+                _fill_mc_row(self.mc_table.row, mc_event)
 
     def flush(self) -> None:
         """Flush the basic file components.
