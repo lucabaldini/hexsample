9c659c45ed18633a51aeb0435622af819ca9f51a
Augusto Cattafesta <augustocattafesta@gmail.com>
2026-04-29 17:11:49 +0200

    Added method to verify if pixel is inside hexagonal grid.

--- src/hexsample/hexagon.py
+++ src/hexsample/hexagon.py
@@ -383,7 +383,21 @@ class HexagonalGrid:
             row = r + (q + self._parity_offset(q)) // 2
         return col, row
 
-    def world_to_pixel(self, x: np.array, y: np.array) -> Tuple[np.array, np.array]:
+    def _is_in_bounds(self, col: np.array, row: np.array) -> np.array:
+        """Check if the given pixels are inside the hexagonal grid bounds.
+
+        Arguments
+        ---------
+        col : array_like
+            The column pixel coordinate(s).
+
+        row : array_like
+            The row pixel coordinate(s).
+        """
+        return (0 <= col) & (col < self.num_cols) & (0 <= row) & (row < self.num_rows)
+
+    def world_to_pixel(self, x: np.array, y: np.array,
+                       in_bounds: bool = False) -> Tuple[np.array, np.array]:
         """Transform world coordinates to pixel coordinates.
 
         This proceeds in three basic steps (conversion to fractional axial coordinates,
@@ -398,6 +412,10 @@ class HexagonalGrid:
 
         y : array_like
             The input y coordinate(s).
+        
+        in_bounds : bool, optional
+            If True, mask the output arrays to return only the pixels that are inside
+            the grid bounds.
         """
         # pylint: disable = invalid-name
         # Add back the offsets---and remember the y axis is running from the
@@ -411,6 +429,12 @@ class HexagonalGrid:
         q, r = self._axial_round(q, r)
         # ... and convert to offset coordinates.
         col, row = self._axial_to_offset(q, r)
+        # If requested, mask the output arrays to return only the pixels that are inside
+        # the grid bounds.
+        if in_bounds:
+            mask = self._is_in_bounds(col, row)
+            col = col[mask]
+            row = row[mask]
         return col, row
 
     def pixel_logical_coordinates(self) -> Tuple[np.array, np.array]:
