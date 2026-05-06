c84396f62ff6790035712e8e9307d95741917537
Luca Baldini <luca.baldini@pi.infn.it>
2026-05-06 15:09:50 +0200

    New stats module with the running stats

---
+++ src/hexsample/stats.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2026 the hexsample team.
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""Statistical tools.
+"""
+
+from typing import Tuple, Union
+
+import numpy as np
+
+
+class RunningStats:
+
+    """Small convenience class to accumulate running statistics (mean and variance)
+    of a stream of data.
+    """
+
+    def __init__(self, shape: Union[int, Tuple[int, ...]] = ()) -> None:
+        """Constructor.
+        """
+        self._mean = np.zeros(shape, dtype=np.float64)
+        self._counts = np.zeros(shape, dtype=np.int64)
+        self._m2 = np.zeros(shape, dtype=np.float64)
+
+    def update(self, val: np.ndarray, *indices: np.ndarray) -> None:
+        """Update the running stats.
+
+        Arguments
+        ---------
+        val : array-like
+            The new value(s) to be included in the running stats.
+
+        *indices : array-like
+            The indices of the elements to update. (Note the shape of the arguments
+            must match that of the values for the update.)
+        """
+        self._counts[*indices] += 1
+        delta = val - self._mean[*indices]
+        self._mean[*indices] += delta / self._counts[*indices]
+        self._m2[*indices] += delta * (val - self._mean[*indices])
+
+    def mean(self) -> np.ndarray:
+        """Return the current value for the mean.
+        """
+        return self._mean
+
+    def var(self, ddof: int = 1):
+        """Return the current value for the variance.
+
+        Arguments
+        ---------
+        ddof : int
+            Delta degrees of freedom (default is 1 for the unbiased sample variance.)
+        """
+        return self._m2 / (self._counts - ddof)
+
+    def std(self, ddof: int = 1):
+        """Return the current value for the standard deviation.
+
+        Arguments
+        ---------
+        ddof : int
+
+        """
+        return np.sqrt(self.var(ddof))
---
+++ tests/test_stats.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2026 the hexsample team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Test suite for hexsample.stats
+"""
+
+import numpy as np
+
+from hexsample import rng
+from hexsample.stats import RunningStats
+
+rng.initialize(seed=666)
+
+
+def test_running_stats_scalar():
+    """Test the scalar version of the running stats.
+    """
+    running_stats = RunningStats()
+    data = rng.generator.normal(size=1000)
+    for val in data:
+        running_stats.update(val)
+    assert np.isclose(running_stats.mean(), np.mean(data))
+    assert np.isclose(running_stats.var(), np.var(data, ddof=1))
+    assert np.isclose(running_stats.std(), np.std(data, ddof=1))
+
+
+def test_running_stats_1d(shape=10):
+    """Test the 1D version of the running stats.
+    """
+    # Generate some random data.
+    data = rng.generator.normal(size=(1000, shape))
+    # Tier 1: update all the indices in the underlying array.
+    running_stats = RunningStats(shape)
+    indices = np.arange(shape)
+    for val in data:
+        running_stats.update(val, indices)
+    assert np.allclose(running_stats.mean(), np.mean(data, axis=0))
+    assert np.allclose(running_stats.var(), np.var(data, axis=0, ddof=1))
+    assert np.allclose(running_stats.std(), np.std(data, axis=0, ddof=1))
+    # Tier 2: only update the first half of the underlying array.
+    running_stats = RunningStats(shape)
+    lim = shape // 2
+    indices = np.arange(lim)
+    for val in data:
+        running_stats.update(val[:lim], indices)
+    assert np.allclose(running_stats.mean()[:lim], np.mean(data[:, :lim], axis=0))
+    assert np.allclose(running_stats.mean()[lim:], 0.)
+    assert np.allclose(running_stats.var()[:lim], np.var(data[:, :lim], axis=0, ddof=1))
+    assert np.allclose(running_stats.var()[6:], 0.)
+
+
+def test_running_stats_2d(shape=(3, 3)):
+    """Test the 2D version of the running stats.
+    """
+    # Generate some random data.
+    data = rng.generator.normal(size=(1000, *shape))
+    # Tier 1: update all the indices in the underlying array.
+    running_stats = RunningStats(shape)
+    rows, cols = np.nonzero(np.ones(shape))
+    for val in data:
+        running_stats.update(val[rows, cols], cols, rows)
+    print(running_stats.mean())
+    print(np.mean(data, axis=0))
+    #assert np.allclose(running_stats.mean(), np.mean(data, axis=0))
+    #assert np.allclose(running_stats.var(), np.var(data, axis=0, ddof=1))
+    #assert np.allclose(running_stats.std(), np.std(data, axis=0, ddof=1))
